76ee6d945c6b4b11c74961d56817bcc648598b04
Alexey Glushkov <soniccat@list.ru>
2017-09-24 16:03:18 +0300

    it works

--- bar.js
+++
@@ -1,8 +0,0 @@
-
-
-
-function bar() {
-	setTimeout(function() { require('./my2.scss'); }, 1000);
-}
-
-module.exports = bar;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './app.js',
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
