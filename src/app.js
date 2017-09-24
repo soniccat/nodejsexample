88628a6b3cc6986f274697a47104e53982790a31
Alexey Glushkov <soniccat@list.ru>
2017-09-24 16:07:29 +0300

    it works

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 var bar = require('./bar');
+var image = require('./screen.png');
 
 function addElement() {
     var element = document.createElement('div');
-
-    element.innerHTML = "<b>Text</b><img src='./screen.png'/>";
+    element.innerHTML = "<b>Text</b>";
     element.classList.add('hello');
 
     document.body.appendChild(element);
+
+    var element2 = new Image();
+    element2.src = image;
+    document.body.appendChild(element2);
 }
 
 bar();
