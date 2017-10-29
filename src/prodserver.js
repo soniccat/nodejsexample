6ae39bb6bfdebb87651f8d2ef42f653b60c780d2
Alexey Glushkov <soniccat@list.ru>
2017-10-29 10:47:29 +0300

    added prodserver

---
+++ src/prodserver.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+var express = require('express');
+var app = express();
+//var content = fs.readFileSync('./dist/index.html');
+
+app.use(express.static('./dist'));
+app.get('/mmm', function (req, res) {
+    res.header("Content-Type", "text/html");
+    res.send("override");
+});
+
+app.listen(80, function () {
+
+});
