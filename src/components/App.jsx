01b75ceb8039f909cdd9728ed003ddc619280dd6
Alexey Glushkov <soniccat@list.ru>
2017-11-15 22:05:15 +0300

    initial react structure

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RequestRow from '../elements/RequestRow.jsx'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <RequestRow />
+            </div>
+        );
+    }
+}
+
+export default App
