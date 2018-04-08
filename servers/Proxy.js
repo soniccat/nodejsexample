3bd05f7de354069dc53f5fbfb08dab63b7650fbc
Alexey Glushkov <soniccat@list.ru>
2018-04-08 14:18:44 +0300

    refactoring

--- servers/Proxy.js
+++ servers/Proxy.js
@@ -7,10 +7,8 @@ let gzip = zlib.createGzip();
 
 class Proxy {
 
-    // callback
-    //
     handleRequest(originalRequest, originalResponse, callback) {
-        this.prepareSendRequestInfo(originalRequest,  sendRequestInfo => {
+        this.prepareRequestInfo(originalRequest, sendRequestInfo => {
             this.prepareResponseInfo(sendRequestInfo, responseInfo => {
                 logRequest(sendRequestInfo, responseInfo);
                 originalResponse.writeHead(responseInfo.statusCode , responseInfo.headers);
@@ -26,36 +24,68 @@ class Proxy {
         });
     }
 
-    prepareSendRequestInfo(originalRequest, callback) {
+    prepareRequestInfo(originalRequest, callback) {
         let options = this.getRequestOptions(originalRequest);
 
         // is used to build a db insert query
         // contains options and body keys
-        let sendData = {
+        let requestInfo = {
             options: options
         };
 
         readPostBody(originalRequest, body => {
             if (body) {
-                sendData.body = body;
+                requestInfo.body = body;
             }
 
-            callback(sendData);
+            callback(requestInfo);
         });
+    }
+
+    getRequestOptions(req) {
+        let reqUrl = url.parse(req.url);
+        let redirectHost = 'news360.com';
+        let needRedirect = reqUrl.host == undefined || reqUrl.host == "localhost";
+        let host = needRedirect ? redirectHost : reqUrl.host;
+        let path = reqUrl.path;
+        let defaultHeaders = req.headers;
+
+        defaultHeaders["accept-encoding"] = "";
+        if (needRedirect) {
+            defaultHeaders["host"] = redirectHost;
+        }
+
+        // to avoid caching
+        delete defaultHeaders["if-modified-since"];
+        delete defaultHeaders["if-none-match"];
+        delete defaultHeaders["origin"];
+        delete defaultHeaders["referer"];
+
+        let headers = needRedirect ? defaultHeaders : req.headers;
+
+        let options = {
+            host: host,
+            port: 443,
+            path: path,
+            method: req.method,
+            headers: headers
+        };
+
+        return options;
     }
 
     prepareResponseInfo(sendRequestInfo, callback) {
         // is used to build a db insert query
         // contains headers, statusCode and body keys
-        var responseInfo = {
+        let responseInfo = {
         };
 
-        var creq = https.request(sendRequestInfo.options, cres => {
-            var headers = this.buildPoxyHeaders(cres);
+        let creq = https.request(sendRequestInfo.options, cres => {
+            let headers = this.buildPoxyHeaders(cres);
             responseInfo.headers = headers;
             responseInfo.statusCode = cres.statusCode;
 
-            var chunks = [];
+            let chunks = [];
             cres.on('data', chunk => {
                 chunks.push(chunk);
             });
@@ -65,7 +95,7 @@ class Proxy {
             });
 
             cres.on('end', () => {
-                var buffer = Buffer.concat(chunks);
+                let buffer = Buffer.concat(chunks);
                 this.handleRequestEnd(cres, buffer, data => {
                     responseInfo.body = data;
                     callback(responseInfo);
@@ -85,43 +115,11 @@ class Proxy {
     }
 
     buildPoxyHeaders(cres) {
-        var headers = cres.headers;
+        let headers = cres.headers;
         if (cres.headers["content-type"]) {
             headers["Content-Type"] = cres.headers["content-type"];
         }
         return headers;
-    }
-
-    getRequestOptions(req) {
-        var reqUrl = url.parse(req.url);
-        var redirectHost = 'news360.com';
-        var needRedirect = reqUrl.host == undefined || reqUrl.host == "localhost";
-        var host = needRedirect ? redirectHost : reqUrl.host;
-        var path = reqUrl.path;
-        var defaultHeaders = req.headers;
-
-        defaultHeaders["accept-encoding"] = "";
-        if (needRedirect) {
-            defaultHeaders["host"] = redirectHost;
-        }
-
-        // to avoid caching
-        delete defaultHeaders["if-modified-since"];
-        delete defaultHeaders["if-none-match"];
-        delete defaultHeaders["origin"];
-        delete defaultHeaders["referer"];
-
-        var headers = needRedirect ? defaultHeaders : req.headers;
-
-        var options = {
-            host: host,
-            port: 443,
-            path: path,
-            method: req.method,
-            headers: headers
-        };
-
-        return options;
     }
 
     handleRequestEnd(request, buffer, callback) {
@@ -131,9 +129,9 @@ class Proxy {
     }
 
     handleGzip(cres, buffer, completion) {
-        var contentEncoding = cres.headers['content-encoding'];
+        let contentEncoding = cres.headers['content-encoding'];
         if (contentEncoding) {
-            var isGzip = contentEncoding.indexOf("gzip") != -1 || contentEncoding.indexOf("deflate") != -1;
+            let isGzip = contentEncoding.indexOf("gzip") != -1 || contentEncoding.indexOf("deflate") != -1;
             if (isGzip) {
                 zlib.unzip(buffer, (err, decoded) => {
                     console.log("decoding...");
