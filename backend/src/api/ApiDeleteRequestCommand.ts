43028f4b5043ca0f00d60c4e6c7c00b0de17d5a3
Alexey Glushkov <soniccat@list.ru>
2018-08-18 22:28:35 +0300

    added missed file

---
+++ backend/src/api/ApiDeleteRequestCommand.ts
@@ -0,0 +1,49 @@
+import { ApiCommand, setResponseHeader } from 'main/api/ApiCommand';
+import ApiRequestInfo from 'main/api/ApiRequestInfo';
+import * as http from 'http';
+import * as util from 'util';
+import RequestTable, { RequestRow } from 'main/RequestTable';
+import ILogger, { LogLevel } from 'main/logger/ILogger';
+
+// SPEC:
+//
+// request/id  (POST)- create request
+// params:
+//  json representation of an object
+
+export default class ApiDeleteRequestCommand implements ApiCommand {
+  requestTable: RequestTable;
+  logger: ILogger;
+
+  constructor(requestTable: RequestTable, logger: ILogger) {
+    this.requestTable = requestTable;
+    this.logger = logger;
+  }
+
+  async run(requestInfo: ApiRequestInfo, res: http.ServerResponse): Promise<http.ServerResponse> {
+    const id = parseInt(requestInfo.components[1], 10);
+    await this.handleDeleteRequest(id, res);
+    return res;
+  }
+
+  canRun(requestInfo: ApiRequestInfo): boolean {
+    return requestInfo.components.length === 2 &&
+      requestInfo.body === undefined &&
+      requestInfo.method === 'DELETE' &&
+      requestInfo.components[0] === 'request';
+  }
+
+  async handleDeleteRequest(id: number, res: http.ServerResponse): Promise<http.ServerResponse> {
+    return this.requestTable.deleteRequestRow(id)
+    .then(() => {
+      setResponseHeader(res, 200, '');
+    })
+    .catch((err) => {
+      this.logger.log(LogLevel.ERROR, `ApiDeleteRequestCommand.handleDeleteRequest error: ${util.inspect(err)}`);
+      setResponseHeader(res, 500);
+    })
+    .then(() => {
+      return res;
+    });
+  }
+}
